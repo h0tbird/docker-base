9ebb9939f2932068b6f10316e0399fcbdf3a125f
Marc Villacorta <marc.villacorta@gmail.com>
2014-07-05 14:37:24 +0200

    New include statement

--- puppet/environments/production/manifests/site.pp
+++ puppet/environments/production/manifests/site.pp
@@ -9,12 +9,10 @@ stage { 'post': require => Stage['main'] }
 # Enable the Puppet 4 behavior today:
 #------------------------------------------------------------------------------
 
-Package {
-  allow_virtual => true,
-}
+Package { allow_virtual => true }
 
 #------------------------------------------------------------------------------
 # Include:
 #------------------------------------------------------------------------------
 
-include r_base::container
+include "::r_base::${::virtual}"
