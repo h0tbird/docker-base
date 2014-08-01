7da43d17bcb1d0f4a67dff106c01e7c12f5624c6
Marc Villacorta <marc.villacorta@gmail.com>
2014-08-01 19:11:09 +0200

    No need to mount cgroup anymore

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SUDO=`which sudo`; if [[ $EUID -ne 0 ]]; then $SUDO $0 $@; else
-  CID=$(docker run --privileged -d -P -v /sys/fs/cgroup:/sys/fs/cgroup:ro h0tbird/base)
+  CID=$(docker run --privileged -d -P h0tbird/base)
 fi
