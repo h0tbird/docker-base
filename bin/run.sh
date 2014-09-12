0f65450755d6078537c1e63fd58896504aacfa44
Marc Villacorta <marc.villacorta@gmail.com>
2014-09-13 00:15:07 +0200

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 SUDO=`which sudo`; if [[ $EUID -ne 0 ]]; then $SUDO $0 $@; else
-  CID=$(docker run --privileged -d -P h0tbird/base)
+
+  ROLE='base'
+  DOMAIN='demo.lan'
+
+  LIST=`docker ps -a | awk '/'$ROLE'[0-9]/ {print $NF}'`
+  for ID in `seq -w 99`; do echo $LIST | grep -q $ID || break; done
+  NAME=${ROLE}${ID}.${DOMAIN}
+
+  CID=$(docker run --privileged -h $NAME --name $NAME -d -P h0tbird/base)
+
 fi
